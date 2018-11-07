bd58c965eebcb73fdf358efe146c3c76809f2b0e
Georgios Giasemidis <g.giasemidis@gmail.com>
2018-11-07 02:23:22 +0000

    pandas implementation finished, 2016 f4 teams added, check of misspelled teams done

--- analysis.py
+++ analysis.py
@@ -4,17 +4,50 @@ Created on Sun Apr  1 16:27:06 2018
 
 @author: Georgios
 """
+'''
+Performs simple analysis and evaluates the scoring of simple benchmark models:
+    1) Home team always wins
+    2) F4 teams alway win when playing with a non-F4 team, 
+        otherwise home team always wins.
+    3) Persistence model, teams that won in the previoous round win, 
+        if both teams have won, home team wins.
+    4) Standing model, team higher in the standings wins.
+    5) Panathinaikos always wins, otherwise home team always wins
+    6) Random model. 
+'''
 import numpy as np
-#from auxiliary.io_csv import read_csv
 import pandas as pd
+import sys
 
-#data, header = read_csv('data/euroleague_results_2017.csv')
-data = pd.read_csv('data/euroleague_results_2017.csv')
-standings = pd.read_csv('data/euroleague_standings_2017.csv')
+# year (start of season)
+year = 2016
+data = pd.read_csv('data/euroleague_results_%d.csv' % year)
+standings = pd.read_csv('data/euroleague_standings_%d.csv' % year)
 
-#f4Teams = ['Fenerbahce Istanbul', 'BC Olympiakos Piraeus', 'CSKA Moscow', 'Real Madrid BC']
-f4Teams = ['Fenerbahce Dogus Istanbul', 'Olympiacos Piraeus', 
-           'CSKA Moscow', 'Real Madrid']
+#%%
+if year == 2017:
+    # f4 teams in 2016-2017 year
+    f4Teams = ['Fenerbahce Dogus Istanbul', 'Olympiacos Piraeus', 
+               'CSKA Moscow', 'Real Madrid']
+elif year == 2016:
+    # f4 teams in 2015-2016 year
+    f4Teams = ['Fenerbahce Dogus Istanbul', 'CSKA Moscow',
+              'Lokomotiv Kuban', 'Baskonia Vitoria Gasteiz']
+else:
+    sys.exit()
+
+#%% Checks
+
+stand_teams = np.unique(standings['Club Name'])
+resul_teams = np.unique(data['Home Team'])
+if not np.in1d(stand_teams, resul_teams).all():
+    ii = ~np.in1d(stand_teams, resul_teams)
+    print(stand_teams[ii])
+if not np.in1d(resul_teams, stand_teams).all():
+    ii = ~np.in1d(resul_teams, stand_teams)
+    print(resul_teams[ii])
+
+#%%
 
 teams = np.unique((data['Home Team'].values))
 nmatches = data.shape[0]
@@ -25,60 +58,51 @@ team_dict = dict.fromkeys(teams, np.ones(ngames, dtype=int))
 data['Actual'] = np.where(data['Home Score'].values > data['Away Score'].values, 1, 2)
 data['Home Wins'] = np.ones(nmatches, dtype=int)
 
+# f4 model
 f4wins = np.ones(nmatches, dtype=int)
 hmf4 = np.in1d(data['Home Team'].values, f4Teams)
 awf4 = np.in1d(data['Away Team'].values, f4Teams)
 f4wins[awf4 & (~hmf4)] = 2
 data['F4 Wins'] = f4wins
 
-#persistence =  np.ones(nmatches)
-#
-#stand = np.ones(nmatches, dtype=int)
-#for r in np.unique(data['Round']):
-#    if r == 1:
-#        continue
-#    s = standings[standings['Round'] == r]
-#    stand[(data['Round'] == r) & (s['Club Name']==data[]]
-#    
-#
+#%%
+
+persistence =  np.ones(nmatches, dtype=int)
+stand = np.ones(nmatches, dtype=int)
+for r in np.unique(data['Round']):
+    if r == 1:
+        continue
+    
+    # standings model
+    s = standings[standings['Round']==r-1]
+    d = data[data['Round']==r]
+    
+    home_stands = np.array([s[s['Club Name']==u]['Position'].iloc[0] 
+                            for u in d['Home Team'].values])
+    away_stands = np.array([s[s['Club Name']==u]['Position'].iloc[0] 
+                            for u in d['Away Team'].values])
+    stand[data['Round'] == r] = np.where(home_stands < away_stands, 1, 2)
+
+    # persistence model
+    if r == 2:
+        home_won = np.array([1 if s[s['Club Name']==u]['Wins'].iloc[0] > 0
+                        else 0 for u in d['Home Team'].values])
+        away_won = np.array([1 if s[s['Club Name']==u]['Wins'].iloc[0] > 0
+                        else 0 for u in d['Away Team'].values])
+    else:
+        s_prev = standings[standings['Round']==r-2]
+        home_won = np.array([1 if s[s['Club Name']==u]['Wins'].iloc[0] > 
+                             s_prev[s_prev['Club Name']==u]['Wins'].iloc[0] 
+                             else 0 for u in d['Home Team'].values])
+        away_won = np.array([1 if s[s['Club Name']==u]['Wins'].iloc[0] > 
+                             s_prev[s_prev['Club Name']==u]['Wins'].iloc[0] 
+                             else 0 for u in d['Away Team'].values])
+    persistence[data['Round'] == r] = np.where(away_won > home_won, 2, 1)
+    
+
+data['Standings'] = stand
+data['Persistence'] = persistence
 data['Pana'] = np.where(data['Away Team']=='Panathinaikos Superfoods Athens', 2, 1)
-#
-##team_dict = dict.fromkeys(teams, np.zeros(ngames))
-#team_dict = {k: {'wl': np.zeros(ngames, dtype=int),
-#                 'scored': np.zeros(ngames, dtype=int),
-#                 'conceded': np.zeros(ngames, dtype=int)}
-#             for k in teams}
-#
-#for i, match in enumerate(data):
-#
-#    gd = int(match[3])
-#    ii = gd - 1
-#    team_dict[match[1]]['wl'][ii] = 1 if int(match[5]) > int(match[6]) else 0
-#    team_dict[match[2]]['wl'][ii] = 1 if int(match[6]) > int(match[5]) else 0
-#
-#    team_dict[match[1]]['scored'][ii] = match[5]
-#    team_dict[match[1]]['conceded'][ii] = match[6]
-#    team_dict[match[2]]['scored'][ii] = match[6]
-#    team_dict[match[2]]['conceded'][ii] = match[5]
-#
-#    # persistence model
-#    if gd > 1 and team_dict[match[1]]['wl'][ii-1] > team_dict[match[2]]['wl'][ii-1]:
-#        persistence[i] = 1
-#    elif gd > 1 and team_dict[match[2]]['wl'][ii-1] > team_dict[match[1]]['wl'][ii-1]:
-#        persistence[i] = 2
-#    else:
-#        persistence[i] = 1
-#
-#    # standing model (the higher ranking team wins, if tied home wins)
-#    if gd > 1  and np.sum(team_dict[match[2]]['wl'][:ii]) > np.sum(team_dict[match[1]]['wl'][:ii]):
-#        standing[i] = 2
-#        standing2[i] = 2
-#    elif gd > 1 and np.sum(team_dict[match[2]]['wl'][:ii]) == np.sum(team_dict[match[1]]['wl'][:ii]):
-#        home_diff = np.sum(team_dict[match[1]]['scored'][:ii]) -\
-#                          np.sum(team_dict[match[1]]['conceded'][:ii])
-#        away_diff = np.sum(team_dict[match[2]]['scored'][:ii]) -\
-#                          np.sum(team_dict[match[2]]['conceded'][:ii])
-#        standing2[i] = 2 if away_diff > home_diff else 1
 
 random = np.zeros(1000)
 for i in range(1000):
@@ -87,8 +111,7 @@ for i in range(1000):
 
 print('Home wins  :', np.sum(data['Actual'].values==data['Home Wins']))
 print('Top4 wins  :', np.sum(data['Actual'].values==data['F4 Wins']))
-#print('Persistance:', np.sum(data['Actual'].values==persistence))
-#print('Standing   :', np.sum(data['Actual'].values==standing))
-#print('Standing2  :', np.sum(data['Actual'].values==standing2))
+print('Persistance:', np.sum(data['Actual'].values==data['Persistence']))
+print('Standing   :', np.sum(data['Actual'].values==data['Standings']))
 print('Pana       :', np.sum(data['Actual']==data['Pana']))
 print('Random     :', np.round(np.mean(random), 0))
