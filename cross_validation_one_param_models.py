55f91ebaa3d27fed0968983f781e614069f0ada5
Georgios Giasemidis <g.giasemidis@gmail.com>
2018-11-19 02:01:11 +0000

    one param models file renamed

--- cross_validation_one_param_models.py
+++ cross_validation_one_param_models.py
@@ -18,27 +18,45 @@ from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.metrics import accuracy_score
 from matplotlib import pyplot as plt
 from auxiliary.sample_weights import sample_weights
+from auxiliary.fix_team_names import fix_team_names
 
-df = pd.read_csv('data/match_level_features.csv')
-df2 = pd.read_csv('data/euroleague_season_2018_results-end.csv')
+def normalise(X):
+    x_norm = (X - X.min(axis=0)) / (X.max(axis=0) - X.min(axis=0))
+    return x_norm
 
-yy = df2['Home Team ID']
+norm = True
+shuffle = False
+merge = True
+method = 'naive-bayes'
+min_round = 5
 
-y = df['label'].values
-X = df.iloc[:, 2:].values
+print('norm: %r - shuffle: %r - merge: %r - method: %s' % 
+      (norm, shuffle, merge, method))
 
-#ii = X.sum(axis=1) == -X.shape[1]
-#y = y[~ii]
-#X = X[~ii, :]
-#yy = yy[~ii]
+df1 = pd.read_csv('data/match_level_features_2016_2017.csv')
+df2 = pd.read_csv('data/match_level_features_2017_2018.csv')
 
-kk = 40
-y = y[kk:]
-X = X[kk:, :]
-yy = yy [kk:]
+#df1, df2 = fix_team_names(df1, df2)
+#%%
 
-method = 'svm-linear'
+n = list(df1.columns).index('Label')
+ii = df1['Round'].values > min_round
+y_train = df1[ii]['Label'].values
+X_train = df1.iloc[ii, (n+1):].values
 
+n = list(df2.columns).index('Label')
+ii = df2['Round'].values > min_round
+y_test = df2[ii]['Label'].values
+X_test = df2.iloc[ii, (n+1):].values
+
+if merge:
+    X_train = np.concatenate((X_train, X_test), axis=0)
+    y_train = np.concatenate((y_train, y_test), axis=0)
+
+if norm:
+    X_train = normalise(X_train)
+    X_test = normalise(X_test)
+    
 if method == 'log-reg':
     params = np.sort(np.concatenate((np.logspace(-5, 8, 14),
                                      5*np.logspace(-5, 8, 14)), axis=0))
@@ -56,38 +74,38 @@ elif method == 'gradient-boosting':
 else:
     sys.exit('Method not recognised')
 
-skfold = StratifiedKFold(n_splits=5, shuffle=False, random_state=10)
+skfold = StratifiedKFold(n_splits=5, shuffle=shuffle, random_state=10)
 
 accuracy = np.zeros((skfold.get_n_splits(), params.shape[0]))
 w_accuracy = np.zeros((skfold.get_n_splits(), params.shape[0]))
 
 i = -1
-for train_index, test_index in skfold.split(X, yy):
+for train_index, test_index in skfold.split(X_train, y_train):
     i+=1
     print(i)
-    X_train, X_test = X[train_index], X[test_index]
-    y_train, y_test = y[train_index], y[test_index]
-    w = sample_weights(y_test-1, 2)
+    X_train_folds, X_test_fold = X_train[train_index, :], X_train[test_index, :]
+    y_train_folds, y_test_fold = y_train[train_index], y_train[test_index]
+    w = sample_weights(y_test_fold-1, 2)
     
     for j, param in enumerate(params):
     
         if method == 'log-reg':
             model = LogisticRegression(C=param, class_weight='balanced')
         elif method == 'svm-linear':
-            model = SVC(C=param, kernel='linear')
+            model = SVC(C=param, kernel='linear', class_weight='balanced')
         elif method == 'decision-tree':
-            model = DecisionTreeClassifier()
+            model = DecisionTreeClassifier(class_weight='balanced')
         elif method == 'random-forest':
-            model = RandomForestClassifier(n_estimators=param)
+            model = RandomForestClassifier(n_estimators=param, class_weight='balanced')
         elif method == 'naive-bayes':
             model = GaussianNB()
         elif method == 'gradient-boosting':
             model = GradientBoostingClassifier(n_estimators=param)
 
-        model.fit(X_train, y_train)
-        y_pred = model.predict(X_test)
-        accuracy[i, j] = accuracy_score(y_test, y_pred)
-        w_accuracy[i, j] = accuracy_score(y_test, y_pred, sample_weight=w)
+        model.fit(X_train_folds, y_train_folds)
+        y_pred = model.predict(X_test_fold)
+        accuracy[i, j] = accuracy_score(y_test_fold, y_pred)
+        w_accuracy[i, j] = accuracy_score(y_test_fold, y_pred, sample_weight=w)
 
 if params.shape[0] > 1:
     plt.figure()
