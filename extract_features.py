4db526c6d9e8c1811be6c3d6b1750ce2202b6a57
Georgios Giasemidis <g.giasemidis@gmail.com>
2019-05-12 03:03:31 +0100

    flake8 changes

--- extract_features.py
+++ extract_features.py
@@ -11,16 +11,18 @@ from auxiliary.make_features import make_features_from_df
 from auxiliary.make_features import make_team_features
 from auxiliary.io_json import read_json
 
+
 def main(year):
     '''
-    Extract features (game and team) from the fetched data from the 
+    Extract features (game and team) from the fetched data from the
     Euroleague's site
     '''
 
     data = pd.read_csv('data/euroleague_results_%d_%d.csv' % (year, year+1))
-    standings = pd.read_csv('data/euroleague_standings_%d_%d.csv' % (year, year+1))
+    standings = pd.read_csv('data/euroleague_standings_%d_%d.csv'
+                            % (year, year+1))
     f4teams = read_json('data/f4teams.json')
-    
+
     # Specify the F4 teams of the previous year
     f4Teams = f4teams[str(year)]
 
@@ -28,24 +30,25 @@ def main(year):
 
     # make game features
     feats = make_features_from_df(data, standings, f4Teams)
-    
+
     feats.insert(3, 'Label', label)
     # save features to file.
     feats.to_csv('data/match_level_features_%d_%d-2.csv' % (year, year+1),
                  index=False)
-    
-    # make team features  
+
+    # make team features
     team_feats = make_team_features(data, standings, f4Teams, year)
-    #save features to file
-    team_feats.to_csv('data/team_level_features_%d_%d-2.csv' % (year, year+1), 
+    # save features to file
+    team_feats.to_csv('data/team_level_features_%d_%d-2.csv' % (year, year+1),
                       index=False)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--season', type=int,
                         help="the season year")
     args = parser.parse_args()
-    
+
     if args.season is None:
         parser.print_help()
     else:
