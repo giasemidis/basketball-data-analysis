894ed111538c5d93e4c37675f6716749e18cf0fb
Georgios Giasemidis <g.giasemidis@gmail.com>
2019-06-16 21:39:58 +0100

    more plots, fixes

--- data-analysis/descriptive-analysis.py
+++ data-analysis/descriptive-analysis.py
@@ -84,9 +84,8 @@ df3.insert(1, 'Season', 2019)
 df = pd.concat([df1, df2, df3], ignore_index=False)
 df.reset_index(drop=True, inplace=True)
 
-df['Win'] = np.where(df['Home Score'] > df['Away Score'], 'W', 'L')
-df['Win'] = np.where(df['Home Score'] < df['Away Score'], 'W', 'L')
 df['Game Result'] = np.where(df['Home Score'] > df['Away Score'], 1, 2)
+df['Score Difference'] = np.abs(df['Home Score'] - df['Away Score'])
 
 # %% Reshape the data
 
@@ -99,27 +98,52 @@ df_flat['Team Result'] = np.where(((df_flat['Game Result'] == 1) &
                                   ((df_flat['Game Result'] == 2) &
                                    (df_flat['Loc'] == 'Away')), 'W', 'L')
 
+# %% Stat Table
+
+dfgroup = df_flat.groupby(['Season', 'Loc'])['Score'].mean().unstack('Loc')
+dfgroup.columns = ['Away Mean Score', 'Home Mean Score']
+dfgroup.reset_index(inplace=True)
+
+dff = (df.groupby(['Season', 'Game Result'])['Game Result'].
+       count().unstack('Game Result'))
+dff.columns = ['Home Wins', 'Away Wins']
+dff.reset_index(inplace=True)
+
+dfgroup = dfgroup.merge(dff, on='Season')
+
+dff = (df_flat.groupby(['Season', 'Game Result'])['Score'].
+       mean().unstack('Game Result'))
+dff.columns = ['Home Win Mean Score', 'Away Win Mean Score']
+dff.reset_index(inplace=True)
+
+dfgroup = dfgroup.merge(dff, on='Season')
+
+print(dfgroup)
+
 # %% Stat Plots
 
 fig = px.box(df_flat, x="Season", y="Score", color="Loc", notched=True)
+fig.layout.yaxis.update({'showgrid': True, 'gridcolor': 'rgb(200, 200, 200)'})
 plot(fig, 'plot0')
 
-fig = px.box(df, x="Season", y="Home Score", color="Result", notched=True)
+fig = px.box(df, x="Season", y="Home Score", color="Game Result", notched=True)
+fig.layout.yaxis.update({'showgrid': True, 'gridcolor': 'rgb(200, 200, 200)'})
 plot(fig, 'plot1')
 
-fig = px.box(df, x="Season", y="Away Score", color="Result", notched=True)
+fig = px.box(df, x="Season", y="Away Score", color="Game Result", notched=True)
+fig.layout.yaxis.update({'showgrid': True, 'gridcolor': 'rgb(200, 200, 200)'})
 plot(fig, 'plot2')
 
-# %%
-fig = px.histogram(df_flat, x='Score', y='Score', color='Loc',
-                   barmode='group', histnorm='probability')
+fig = px.box(df, x="Season", y="Score Difference", color="Game Result",
+             notched=True)
+fig.layout.yaxis.update({'showgrid': True, 'gridcolor': 'rgb(200, 200, 200)'})
 plot(fig, 'plot3')
 
 # %% Bar plots
 
 make_bar_plot([df_flat, df_flat[df_flat['Loc'] == 'Home'],
                df_flat[df_flat['Loc'] == 'Away']], ['All', 'Home', 'Away'])
-print('Done')
+
 make_bar_plot([df_flat[df_flat['Season'] == 2017],
                df_flat[df_flat['Season'] == 2018],
                df_flat[df_flat['Season'] == 2019]],
