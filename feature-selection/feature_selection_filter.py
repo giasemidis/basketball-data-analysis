e0919cc16654c4bcb05d1582a098c945c00311ac
Georgios Giasemidis <g.giasemidis@gmail.com>
2019-07-21 23:32:27 +0100

    chi2 test added, heatmap of features ranking plotted

--- feature-selection/feature_selection_filter.py
+++ feature-selection/feature_selection_filter.py
@@ -6,6 +6,7 @@ Created on Sat Feb  2 19:25:38 2019
 """
 import numpy as np
 import sys
+from tqdm import tqdm
 # from sklearn.linear_model import LogisticRegression
 # from sklearn.ensemble import RandomForestClassifier
 # from sklearn.tree import DecisionTreeClassifier
@@ -21,14 +22,39 @@ sys.path.append('auxiliary/')
 from data_processing import load_data, shape_data
 from kfold_crosseval import kfold_crosseval
 
+
+def plot(x, accuracy, w_accuracy, title=''):
+    plt.figure()
+    plt.plot(x, accuracy, label='Accuracy')
+    plt.plot(x, w_accuracy, label='W-Accuracy')
+    plt.xlabel('Number of features')
+    plt.ylabel('Score')
+    plt.xticks(x, x)
+    plt.minorticks_on()
+    plt.grid(which='major', linestyle='-')
+    plt.grid(which='minor', linestyle='--')
+    plt.title(title)
+    plt.legend()
+    plt.show()
+    return
+
+
+def mutual_info_classif2(X, y, discrete_features='auto', n_neighbors=3,
+                         copy=True, random_state=10):
+    return mutual_info_classif(X, y, discrete_features=discrete_features,
+                               n_neighbors=n_neighbors, copy=copy,
+                               random_state=random_state)
+
+
 level = 'match'
 shuffle = True
 norm = True
 min_round = 5
 nsplits = 5
 # mode = GaussianNB()
-model = AdaBoostClassifier(n_estimators=115, random_state=10,
-                           learning_rate=1.1)
+random_state = 10
+model = AdaBoostClassifier(n_estimators=121, random_state=random_state,
+                           learning_rate=1.0)
 
 # %% load data
 df = load_data(level)
@@ -57,8 +83,8 @@ X_train, y_train, df, groups = shape_data(df, feats, norm=norm,
 
 # %% Filter based feature selection (model independent)
 fscores, ps = f_classif(X_train, y_train)
-mscores = mutual_info_classif(X_train, y_train)
-chiscores = chi2(X_train, y_train)
+mscores = mutual_info_classif(X_train, y_train, random_state=10)
+chiscores, _ = chi2(X_train, y_train)
 ordered_fc = [feats[u] for u in np.argsort(fscores)[::-1]]
 ordered_mi = [feats[u] for u in np.argsort(mscores)[::-1]]
 ordered_ch = [feats[u] for u in np.argsort(chiscores)[::-1]]
@@ -67,18 +93,21 @@ print('MI scores', ordered_mi)
 print('Chi scores', ordered_ch)
 
 # %%
-accuracy = np.zeros((n_feats, 2))
-w_accuracy = np.zeros((n_feats, 2))
+accuracy = np.zeros((n_feats, 3))
+w_accuracy = np.zeros((n_feats, 3))
 feats_fs = []
 feats_mi = []
-for i, n in enumerate(range(1, n_feats + 1)):
+for i, n in enumerate(tqdm(range(1, n_feats + 1))):
     kk = n if n < n_feats else 'all'
     skb_fc = SelectKBest(f_classif, k=kk)
-    skb_mi = SelectKBest(mutual_info_classif, k=kk)
+    skb_mi = SelectKBest(mutual_info_classif2, k=kk)
     skb_ch = SelectKBest(chi2, k=kk)
     X_fc = skb_fc.fit_transform(X_train, y_train)
     X_mi = skb_mi.fit_transform(X_train, y_train)
     X_ch = skb_ch.fit_transform(X_train, y_train)
+
+    # print('MI:', skb_mi.scores_)
+    # print(skb_mi.get_support())
 
     accuracy[i, 0], w_accuracy[i, 0] = kfold_crosseval(X_fc, y_train, df,
                                                        nsplits, groups=groups,
@@ -90,29 +119,30 @@ for i, n in enumerate(range(1, n_feats + 1)):
                                                        model=model,
                                                        level=level,
                                                        shuffle=shuffle)
+    accuracy[i, 2], w_accuracy[i, 2] = kfold_crosseval(X_ch, y_train, df,
+                                                       nsplits, groups=groups,
+                                                       model=model,
+                                                       level=level,
+                                                       shuffle=shuffle)
+
 #    feats_fs.append(ordered_fc[:n])
 #    feats_mi.append(ordered_mi[:n])
 # %%
-x = np.arange(1, n_feats + 1)
-plt.figure()
-plt.plot(x, accuracy[:, 0], label='Accuracy')
-plt.plot(x, w_accuracy[:, 0], label='W-Accuracy')
-plt.xticks(x, x)
-plt.minorticks_on()
-plt.grid(which='major', linestyle='-')
-plt.grid(which='minor', linestyle='--')
-plt.title('ANOVA')
-plt.legend()
-plt.show()
 
+x = np.arange(1, n_feats + 1)
+plot(x, accuracy[:, 0], w_accuracy[:, 0], title='ANOVA')
+plot(x, accuracy[:, 1], w_accuracy[:, 1], title='MI')
+plot(x, accuracy[:, 2], w_accuracy[:, 2], title='Chi2')
+
+scores = np.concatenate((fscores[:, None], mscores[:, None],
+                         chiscores[:, None]), axis=1)
+order = np.argsort(scores, axis=0)
+ranks = order.argsort(axis=0)
+
+# %%
 plt.figure()
-plt.plot(x, accuracy[:, 1], label='Accuracy')
-plt.plot(x, w_accuracy[:, 1], label='W-Accuracy')
-plt.ylabel('Score')
-plt.xticks(x, x)
-plt.minorticks_on()
-plt.grid(which='major', linestyle='-')
-plt.grid(which='minor', linestyle='--')
-plt.title('MI')
-plt.legend()
+plt.imshow((scores.shape[0] - ranks).T)
+plt.yticks(ticks=[0, 1, 2], labels=['ANOVA', 'MI', 'Chi2'])
+plt.xticks(ticks=np.arange(len(feats)), labels=feats, rotation='vertical')
+plt.colorbar(orientation='horizontal', pad=0.3)
 plt.show()
