c494f6e9cbfe5f364da4f974ec6684f1b38cdbd4
Georgios Giasemidis <g.giasemidis@gmail.com>
2019-08-04 21:58:19 +0100

    argsparse improvements

--- feature-extraction/extract_features.py
+++ feature-extraction/extract_features.py
@@ -12,6 +12,7 @@ from make_features import make_game_features
 from make_features import make_team_features
 sys.path.append('auxiliary')
 from io_json import read_json
+from argparser_types import is_valid_parent_path
 
 
 def main(year, results_file, standings_file, f4_file, output=''):
@@ -34,34 +35,31 @@ def main(year, results_file, standings_file, f4_file, output=''):
 
     feats.insert(3, 'Label', label)
     # save features to file.
-    feats.to_csv(output + 'match_level_features.csv',
-                 index=False)
+    feats.to_csv(output + 'match_level_features.csv', index=False)
 
     # make team features
     team_feats = make_team_features(data, standings, f4Teams, year)
     # save features to file
-    team_feats.to_csv(output + 'team_level_features.csv',
-                      index=False)
+    team_feats.to_csv(output + 'team_level_features.csv', index=False)
+    return
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--season', type=int,
+    parser.add_argument('-s', '--season', required=True,
+                        type=lambda x: is_valid_parent_path(parser, x),
                         help="starting year of the season")
-    parser.add_argument('-r', '--results', type=str,
+    parser.add_argument('-r', '--results', required=True,
+                        type=lambda x: is_valid_parent_path(parser, x),
                         help="results file of a season")
-    parser.add_argument('-t', '--table', type=str,
+    parser.add_argument('-t', '--table', required=True,
+                        type=lambda x: is_valid_parent_path(parser, x),
                         help="table/standings file of a season")
-    parser.add_argument('-f', '--final_four', type=str,
+    parser.add_argument('-f', '--final_four', required=True,
+                        type=lambda x: is_valid_parent_path(parser, x),
                         help="final four file of teams of previous a season")
-    parser.add_argument('-o', '--output', type=str,
+    parser.add_argument('-o', '--output', type=str, default='',
                         help="prefix of output files")
     args = parser.parse_args()
 
-    if args.season is None or args.results is None or args.table is None\
-            or args.final_four is None:
-        parser.print_help()
-    else:
-        output = '' if args.output is None else args.output
-        main(args.season, args.results, args.table, args.final_four,
-             output)
+    main(args.season, args.results, args.table, args.final_four, args.output)
